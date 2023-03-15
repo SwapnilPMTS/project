82d72006fb932a341c0c7617ea936bc158da4c5e
swapnilPMTS <swapnil.patil@parallel-minds.com>
2023-03-15 14:48:00 +0530

    second file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './User';
-
+import ClassComp from './ClassComp';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world !</h1>
       <User />
+      <ClassComp></ClassComp>
     </div>
   );
 }
